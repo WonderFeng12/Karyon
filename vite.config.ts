1e2553b6593be27291c60cb1597e50b5869f618c
WonderFeng12 <68208648+WonderFeng12@users.noreply.github.com>
2026-04-19 16:12:24 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV || 'development')
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    },
+
+  };
+});
